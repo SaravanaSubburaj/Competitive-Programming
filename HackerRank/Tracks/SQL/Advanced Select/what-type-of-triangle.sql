2121f8068f1762583e39fa903a378b4b0ab11ce8
Saravana Subburaj <saravana.subburaj@thedatateam.in>
2018-06-05 01:23:29 +0530

    Removed all

--- HackerRank/Tracks/SQL/Advanced Select/what-type-of-triangle.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    IF( A+B <= C OR B+C <= A OR C+A <= B , "Not A Triangle",
-CASE
-    WHEN A = B AND B = C THEN "Equilateral"
-    WHEN A = B OR B = C OR C = A THEN "Isosceles"
-    WHEN A != B AND B != C AND C != A THEN "Scalene"
-END )
-FROM TRIANGLES;
--- hackerearth/practice/README.md
+++
@@ -1,1 +0,0 @@
-Practice section problems in HackerEarth
